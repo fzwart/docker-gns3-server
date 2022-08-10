47a2411c28fb51acc244ab0d757a9bb9ce121ab0
Frank Zwart <frank@frankzwart.nl>
2022-08-10 13:23:52 +0200

    make network services optional

--- start.sh
+++ start.sh
@@ -13,8 +13,11 @@ if [ "${BRIDGE_ADDRESS}x" == "x" ]; then
   BRIDGE_ADDRESS=172.21.1.1/24
 fi
 ip ad add ${BRIDGE_ADDRESS} dev virbr0
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-dnsmasq -i virbr0 -z -h --dhcp-range=172.21.1.10,172.21.1.250,4h
+if [ ${NO_NETWORK_SERVICES} != "true" ];then
+  iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+  dnsmasq -i virbr0 -z -h --dhcp-range=172.21.1.10,172.21.1.250,4h
+fi
+
 dockerd --storage-driver=vfs --data-root=/data/docker/ &
 gns3server -A --config /data/config.ini
